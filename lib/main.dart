569d0a46c6248b5588b59a0f5e495c0c5cb2c8b8
Shrijal Shrestha <shrijalshrestha313@gmail.com>
2023-07-05 13:12:00 +0545

    removed unnecessary code/comments & created Countdown App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:countdown/common/config/theme/app_theme.dart';
+import 'package:countdown/module/timer/view/countdown_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,56 +13,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Countdown',
-      theme:AppTheme.lightTheme,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: AppTheme.lightTheme,
+      home: const CountdownApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class CountdownApp extends StatefulWidget {
+  const CountdownApp({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CountdownApp> createState() => _CountdownAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _CountdownAppState extends State<CountdownApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+    return const CountdownScreen();
   }
 }
